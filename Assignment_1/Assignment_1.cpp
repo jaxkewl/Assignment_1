937d98fedcbe92ef7af54f3f622919a1567cbda9
Martin Hong <martinhong79@hotmail.com>
2018-05-30 19:49:17 -0400

    Add project files.

---
+++ Assignment_1/Assignment_1.cpp
@@ -0,0 +1,28 @@
+// Assignment_1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <climits>
+#include <iostream>
+int main()
+{
+	std::cout << CHAR_BIT << " :Number of bits in a char " << std::endl;
+	std::cout << CHAR_MAX << ": Maximum char value " << std::endl;
+	std::cout << CHAR_MIN << ": Minimum char value " << std::endl;
+	std::cout << SHRT_MAX << ": Maximum short value " << std::endl;
+	std::cout << SHRT_MIN << ": Minimum short value " << std::endl;
+	std::cout << USHRT_MAX << ": Maximum unsigned short value " << std::endl;
+	std::cout << INT_MAX << ": Maximum int value " << std::endl;
+	std::cout << INT_MIN << ": Minimum int value " << std::endl;
+	std::cout << UINT_MAX << ": Maximum unsigned int value " << std::endl;
+	std::cout << LONG_MAX << ": Maximum long value " << std::endl;
+	std::cout << LONG_MIN << ": Minimum long value " << std::endl;
+	std::cout << ULONG_MAX << ": Maximum unsigned long value " << std::endl;
+	std::cout << LLONG_MAX << ": Maximum long long value " << std::endl;
+	std::cout << LLONG_MIN << ": Minimum long long value " << std::endl;
+	std::cout << ULLONG_MAX << ": Maximum unsigned long long value " << std::endl;
+
+
+	return 0;
+}
+
