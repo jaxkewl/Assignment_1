28e85ece167f03ecb54a90eb97699b8d0428d49d
Martin Hong <martinhong79@hotmail.com>
2018-05-30 20:03:18 -0400

    added all couts, stubbed functions

--- Assignment_1/Assignment_1.cpp
+++ Assignment_1/Assignment_1.cpp
@@ -4,24 +4,35 @@
 #include "stdafx.h"
 #include <climits>
 #include <iostream>
+
+void swap(double * val1, double * val2)
+{
+
+}
+
+void swap(double & val1, double & val2)
+{
+
+}
+
 int main()
 {
-	std::cout << CHAR_BIT << " :Number of bits in a char " << std::endl;
-	std::cout << CHAR_MAX << ": Maximum char value " << std::endl;
-	std::cout << CHAR_MIN << ": Minimum char value " << std::endl;
-	std::cout << SHRT_MAX << ": Maximum short value " << std::endl;
-	std::cout << SHRT_MIN << ": Minimum short value " << std::endl;
-	std::cout << USHRT_MAX << ": Maximum unsigned short value " << std::endl;
-	std::cout << INT_MAX << ": Maximum int value " << std::endl;
-	std::cout << INT_MIN << ": Minimum int value " << std::endl;
-	std::cout << UINT_MAX << ": Maximum unsigned int value " << std::endl;
-	std::cout << LONG_MAX << ": Maximum long value " << std::endl;
-	std::cout << LONG_MIN << ": Minimum long value " << std::endl;
-	std::cout << ULONG_MAX << ": Maximum unsigned long value " << std::endl;
-	std::cout << LLONG_MAX << ": Maximum long long value " << std::endl;
-	std::cout << LLONG_MIN << ": Minimum long long value " << std::endl;
-	std::cout << ULLONG_MAX << ": Maximum unsigned long long value " << std::endl;
-
+	//using the climit lib, output some class fields and byte sizes of primitives
+	std::cout << CHAR_BIT << " :Number of bits in a char" << ", bytes: " << sizeof(char) << std::endl;
+	std::cout << CHAR_MAX << ": Maximum char value" << ", bytes: " << sizeof(char) << std::endl;
+	std::cout << CHAR_MIN << ": Minimum char value" << ", bytes: " << sizeof(char) << std::endl;
+	std::cout << SHRT_MAX << ": Maximum short value" << ", bytes: " << sizeof(short) << std::endl;
+	std::cout << SHRT_MIN << ": Minimum short value" << ", bytes: " << sizeof(short) << std::endl;
+	std::cout << USHRT_MAX << ": Maximum unsigned short value" << ", bytes: " << sizeof(short) << std::endl;
+	std::cout << INT_MAX << ": Maximum int value" << ", bytes: " << sizeof(int) << std::endl;
+	std::cout << INT_MIN << ": Minimum int value" << ", bytes: " << sizeof(int) << std::endl;
+	std::cout << UINT_MAX << ": Maximum unsigned int value" << ", bytes: " << sizeof(int) << std::endl;
+	std::cout << LONG_MAX << ": Maximum long value" << ", bytes: " << sizeof(long) << std::endl;
+	std::cout << LONG_MIN << ": Minimum long value" << ", bytes: " << sizeof(long) << std::endl;
+	std::cout << ULONG_MAX << ": Maximum unsigned long value" << ", bytes: " << sizeof(long) << std::endl;
+	std::cout << LLONG_MAX << ": Maximum long long value" << ", bytes: " << sizeof(long long) << std::endl;
+	std::cout << LLONG_MIN << ": Minimum long long value" << ", bytes: " << sizeof(long long) << std::endl;
+	std::cout << ULLONG_MAX << ": Maximum unsigned long long value" << ", bytes: " << sizeof(long long) << std::endl;
 
 	return 0;
 }
